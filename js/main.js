28a7558a9035acf2e768a8944ecc8b7a1f2a81e5
masum691 <masumhu691@gmail.com>
2021-08-21 15:15:22 +0600

    completed website

--- js/main.js
+++ js/main.js
@@ -1,72 +1,64 @@
 
 
-// document.getElementById('unified-memory').addEventListener('click', function(){
-//     const memoryCost = document.getElementById('memory-cost');
-//     const memoryCostTotal = memoryCost.innerText;
-//     const result = parseInt(memoryCostTotal) + 188;
-// })
-
-
-function productCost(product,price,isIncreasing){
-    const memoryCost = document.getElementById(product+'-cost');
-    let memoryCostTotal = memoryCost.innerText;
-    if(isIncreasing == true){
-        memoryCostTotal = price;
-    }
-    memoryCost.innerText = memoryCostTotal;
+// total Price calculation 
+function updateTotalPrice() {
+    const bestPrice = parseInt(document.getElementById('best-price').innerText);
+    const memoryCost = parseInt(document.getElementById('memory-cost').innerText);
+    const storageCost = parseInt(document.getElementById('storage-cost').innerText);
+    const DeliveryCharge = parseInt(document.getElementById('delivery-cost').innerText);
+    const totalPrice = bestPrice + memoryCost + storageCost + DeliveryCharge;
+    document.getElementById('total-price').innerText = document.getElementById('overall-price').innerText = totalPrice;
     
-}
+};
+
+// promocode 
+function promoCodeApply() {
+    const promoCode = document.getElementById('promo-code').value;
+    if (promoCode.toLowerCase() == 'stevekaku') {
+        let totalPrice = parseFloat(document.getElementById('total-price').innerText);
+
+        //promocode appling calculation
+        totalPrice = totalPrice - (totalPrice * 0.20);
+        document.getElementById('overall-price').innerText = totalPrice;
+    }
+    else{
+        alert('Unvalid code. Please give a valid promo code.')
+    }
+    document.getElementById('promo-code').value = '';
+};
+
 
 // memory cost
-document.getElementById('firstUnified-memory').addEventListener('click', function(){
-    productCost('memory', 0 ,true);
+document.getElementById('first-unified-memory').addEventListener('click', function () {
+    document.getElementById('memory-cost').innerText = 0;
+    updateTotalPrice();
 });
-document.getElementById('secondUnified-memory').addEventListener('click', function(){
-    productCost('memory', 180 ,true);
+document.getElementById('second-unified-memory').addEventListener('click', function () {
+    const result = document.getElementById('memory-cost').innerText = 180;
+    updateTotalPrice();
 });
 
-// storage cost
-document.getElementById('ssd-storageOne').addEventListener('click', function(){
-    productCost('storage', 0, true);
-})
-document.getElementById('ssd-storageTwo').addEventListener('click', function(){
-    productCost('storage', 100, true);
-})
-document.getElementById('ssd-storageThree').addEventListener('click', function(){
-    productCost('storage', 180, true);
-})
+// storage cost 
+document.getElementById('first-ssd-cost').addEventListener('click', function () {
+    document.getElementById('storage-cost').innerText = 0;
+    updateTotalPrice();
+});
+document.getElementById('second-ssd-cost').addEventListener('click', function () {
+    document.getElementById('storage-cost').innerText = 100;
+    updateTotalPrice();
+});
+document.getElementById('third-ssd-cost').addEventListener('click', function () {
+    document.getElementById('storage-cost').innerText = 180;
+    updateTotalPrice();
+});
 
-// delivery cost
-document.getElementById('late-delivery').addEventListener('click', function(){
-    productCost('delivery', 0, true);
-})
-document.getElementById('fast-delivery').addEventListener('click', function(){
-    productCost('delivery', 20, true);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// delivery charge 
+document.getElementById('free-delivery').addEventListener('click', function () {
+    document.getElementById('delivery-cost').innerText = 0;
+    updateTotalPrice();
+});
+document.getElementById('delivery-charge').addEventListener('click', function () {
+    document.getElementById('delivery-cost').innerText = 20;
+    updateTotalPrice();
+});
 
